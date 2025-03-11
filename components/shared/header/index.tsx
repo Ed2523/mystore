83f3a8c88f04f21da30b52fa2fe7a9a5a9e79d06
Ed2523 <citylight2392@gmail.com>
2025-03-11 11:24:30 -0600

    build of header component

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,5 +1,27 @@
+import { ShoppingCart, UserIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+import styles from "./header.module.scss";
+
 const Header = () => {
-  return <>Header</>;
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navBar}>
+        <Link href='/'>
+          <Image
+            src='/images/logo.svg'
+            alt={`${APP_NAME} logo`}
+            priority={true}
+            height={48}
+            width={48}
+          />
+          <span>{APP_NAME}</span>
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
